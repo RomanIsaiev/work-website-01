f6e7d172ba424b0c013fc462aca317e0dfd2833a
Roman Isaev <roma.isaev.2000@gmail.com>
2023-07-16 23:03:49 +0300

    added six sections

---
+++ js/timer.js
@@ -0,0 +1,47 @@
+function getTimeRemaining(endtime) {
+  var t = Date.parse(endtime) - Date.parse(new Date());
+  var seconds = Math.floor((t / 1000) % 60);
+  var minutes = Math.floor((t / 1000 / 60) % 60);
+  var hours = Math.floor((t / (1000 * 60 * 60)) % 24);
+  return {
+    'total': t,
+    'hours': hours,
+    'minutes': minutes,
+    'seconds': seconds
+  };
+}
+
+function initializeClock(id, endtime) {
+  var clock = document.getElementById(id);
+  var hoursSpan = clock.querySelector('.hours');
+  var minutesSpan = clock.querySelector('.minutes');
+  var secondsSpan = clock.querySelector('.seconds');
+
+  function updateClock() {
+    var t = getTimeRemaining(endtime);
+    hoursSpan.innerHTML = ('0' + t.hours).slice(-2);
+    minutesSpan.innerHTML = ('0' + t.minutes).slice(-2);
+    secondsSpan.innerHTML = ('0' + t.seconds).slice(-2);
+
+    if (t.total <= 0) {
+  clearInterval(timeinterval);
+  var deadline = new Date(Date.parse(endtime) + 24 * 60 * 60 * 1000)
+  initializeClock('countdown', deadline);
+}
+  }
+
+  updateClock();
+  var timeinterval = setInterval(updateClock, 1000);
+}
+
+
+var deadline="July 10 2023 00:00:00 GMT+0300";
+// var deadline = new Date(Date.parse(new Date()) + 24 * 60 * 60 * 1000); //for Ukraine
+// var deadline = new Date(Date.parse(new Date()) + 24 * 60 * 60 * 1000); // for endless timer
+initializeClock('countdown', deadline);
+
+// if (t.total <= 0) {
+//   clearInterval(timeinterval);
+//   var deadline = new Date(Date.parse(endtime) + 2 * 24 * 60 * 60 * 1000);
+//   initializeClock('clockdiv', deadline);
+// }
