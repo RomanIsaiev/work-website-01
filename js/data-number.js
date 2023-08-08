7dc63f5a0b06322b756aabc874e08d1f77a89133
cloudwipe <roma.isaev.2000@gmail.com>
2023-08-08 11:13:08 +0300

    update data

--- js/data-number.js
+++ js/data-number.js
@@ -2,7 +2,7 @@
 var currentDate = new Date();
 
 // Увеличение значения дня на 1
-currentDate.setDate(currentDate.getDate() + 1);
+currentDate.setDate(currentDate.getDate());
 
 // Получение отдельных составляющих даты
 var day = currentDate.getDate();
